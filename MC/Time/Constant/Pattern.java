64ea667872277220b9adf0916186dd1a448d2bb2
haoxinlei <biohow@163.com>
2023-05-01 17:58:54 +0800

    时间工具类 获取指定范围内的日期

---
+++ MC/Time/Constant/Pattern.java
@@ -0,0 +1,13 @@
+package MC.Time.Constant;
+
+import java.time.format.DateTimeFormatter;
+
+public class Pattern {
+
+    public static final DateTimeFormatter YYYYMMDD = DateTimeFormatter.ofPattern("yyyyMMdd");
+    public static final DateTimeFormatter YYYYMM = DateTimeFormatter.ofPattern("yyyyMM");
+    public static final DateTimeFormatter YYYY_MM_DD = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    public static final DateTimeFormatter YYYY_MM = DateTimeFormatter.ofPattern("yyyy-MM");
+
+
+}
