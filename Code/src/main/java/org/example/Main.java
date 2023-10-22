6d08b4aab5eb1f2917bb7386f09b5c1e4831a330
haoxinlei <biohow@163.com>
2023-10-22 13:14:56 +0800

    Code 测试

---
+++ Code/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.Test;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    @Test
+    public void helloWorld(){
+        System.out.println("Hello world!");
+    }
+
+
+
+
+}
